47d9ba7245c731842fa6860ede270ade9dd1e479
CalvinCRFC <CalvinCRFC@gmail.com>
2021-02-18 16:34:26 -0500

    Updated path error

--- scripts/dev_install.sh
+++ scripts/dev_install.sh
@@ -21,10 +21,10 @@ export LIBGL_ALWAYS_INDIRECT=1
 sudo /etc/init.d/dbus start &> /dev/null" >> ".$(basename $SHELL)rc"
 fi
 
-echo "export PATH='\$HOME/.local/bin:\$PATH'" >> ".$(basename $SHELL)rc"
+echo "export PATH=\$HOME/.local/bin:\$PATH" >> ".$(basename $SHELL)rc"
 
 
-# Install ROS Noetic (for now until packages are updated for ROS 2)
+# Install ROS Noetic (for now until all packages are updated for ROS 2)
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
